231cce03a3dc49f61f6795b7bd3330a1e31f309d
newhp <1211amarsingh@gmail.com>
2022-02-11 00:17:11 +0530

    test

--- app/src/main/java/com/kv/userweatherapp/presentation/weather/WeatherFragment.kt
+++ app/src/main/java/com/kv/userweatherapp/presentation/weather/WeatherFragment.kt
@@ -40,6 +40,7 @@ class WeatherFragment : Fragment() {
     }
 
 
+    //
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (isNetworkAvailable(requireContext())) {
